ec5337c9b84d346fb1a559f3391e093a8dd8ae5c
Maulx <damien113@hotmail.ca>
2026-05-14 17:33:30 -0400

    session courante

---
+++ Controllers/NextSessionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Mvc;
+using Models;
+
+public class NextSessionController : Controller
+{
+    public ActionResult Edit()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Edit(int year, string session)
+    {
+        int month = session == "Automne" ? 8 : 1;
+
+        NextSession.CurrentDate = new DateTime(year, month, 2);
+
+        return RedirectToAction("Index", "Students");
+    }
+}
