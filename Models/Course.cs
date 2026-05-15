e26fd9fbfa8fe7e739caaf30397c797ed577f1fe
Beurredepin <94488173+Beurredepin@users.noreply.github.com>
2026-05-15 09:43:08 -0400

    Update Course.cs

--- Models/Course.cs
+++ Models/Course.cs
@@ -36,7 +36,7 @@ namespace Models
         }
         public void UpdateRegistrations(List<int> selectedStudentsId)
         {
-            DeleteAllRegistrations();
+            DeleteNextSessionRegistrations();
             if (selectedStudentsId != null)
                 foreach (int studentId in selectedStudentsId)
                     DB.Registrations.Add(new Registration { StudentId = studentId, CourseId = Id });
